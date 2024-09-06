b526cd3e93c5492b8d646997370684be7920ac27
migungel <migungelnei@hotmail.com>
2024-09-06 13:21:10 -0500

    clase persona

---
+++ src/main/java/com/prueba/ApiClient/Entity/Persona.java
@@ -0,0 +1,24 @@
+package com.prueba.ApiClient.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "personas")
+//@AllArgsConstructor
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private Long id;
+
+    @Column(name="nombre", nullable = false, length = 100)
+    private String nombre;
+    private String genero;
+    private Integer edad;
+
+    @Column(name="identificacion", nullable = false, length = 20)
+    private String identificacion;
+    private String direccion;
+    private String telefono;
+
+}
